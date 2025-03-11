fe52c29831d19f93a19cb585bb717d5b7d6f15bc
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-03-11 18:51:13 +0530

    add first name and last name

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,11 +3,24 @@ import { DownArrow } from "@/utils/icons";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Header = () => {
   const homePage = usePathname() === "/";
   const dashboardPage = usePathname() === "/dashboard";
+  const [user, setUser] = useState({ firstName: "", secondName: "" });
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("formData");
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setUser({
+        firstName: parsedData.firstName,
+        secondName: parsedData.secondName,
+      });
+    }
+  }, []);
+
   return (
     <div className={`py-[19.9px] px-4 ${dashboardPage && "bg-light-white"}`}>
       <div className="max-w-[1140px] container mx-auto justify-between flex">
@@ -28,7 +41,7 @@ const Header = () => {
         <div className="flex cursor-pointer items-center gap-[7px]">
           <Image
             src={
-              !homePage
+              dashboardPage
                 ? "/assets/image/webp/admin-image.webp"
                 : "/assets/image/webp/anymous-profile.webp"
             }
@@ -39,7 +52,9 @@ const Header = () => {
           />
           {!homePage ? (
             <div className="flex flex-col max-sm:hidden gap-[1px]">
-              <p className="font-syne font-medium leading-[100%]">Jhon doe</p>
+              <p className="font-syne font-medium leading-[100%]">
+                {user.firstName} {user.secondName}
+              </p>
               <p className="text-sm text-black/70">Admin</p>
             </div>
           ) : (
